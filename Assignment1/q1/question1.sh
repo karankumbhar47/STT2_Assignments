232db1f3fd483d5a9972b0fe02f6fffed5297d54
karankumbhar47 <karansunilk@iitbhilai.ac.in>
2023-02-17 01:41:15 +0530

    Added question1.sh

---
+++ Assignment1/q1/question1.sh
@@ -0,0 +1,20 @@
+
+#initializing git
+git init
+
+#making subdirectory and 'hello-world' file
+j=1
+for i in 12140860 12140540 12140110
+do
+  dir_name=$i'_member'$j
+  mkdir $dir_name
+  echo 'iitbhilai' >  $dir_name/hello-world
+  j=$((j+1))
+done
+
+#commiting all files
+git add .
+git commit -m"First commit"
+
+#Required git-graph
+git-graph
