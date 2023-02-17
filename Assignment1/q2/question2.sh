06668c7e46135f96f0758a396c104d7d6dde64b6
karankumbhar47 <karansunilk@iitbhilai.ac.in>
2023-02-17 15:05:30 +0530

    question2.sh added

---
+++ Assignment1/q2/question2.sh
@@ -0,0 +1,13 @@
+git init
+j=1
+for i in 12140860  12140540 12140110
+do
+  dir_name=$i'_member'$j
+  mkdir $dir_name
+  echo 'iitbhilai' >  $dir_name/hello-world
+  git add $dir_name/hello-world
+  git commit -m"$dir_name/hello-world created" $dir_name/hello-world 
+  git-graph 
+  sleep 1
+  j=$((j+1))
+done
